3ee8a9ab6760ed186e0fc774b6f480b9db0342a3
Klubochek <46074880+Klubochek@users.noreply.github.com>
2022-11-06 01:47:46 +0200

    First commit

---
+++ KlubochekDiscordBot/KlubochekDiscordBot/Program.cs
@@ -0,0 +1,63 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace KlubochekDiscordBot
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+           => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+
+        private async Task MainAsync()
+        {
+            DiscordSocketClient client = new DiscordSocketClient();
+            string path = @"E:\KlubochekDiscordBot\KlubochekDiscordBot\KlubochekDiscordBot\BotToken.txt";
+            string token;
+
+
+
+            //чтение токена и старт бота;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                token = sr.ReadToEnd().ToString();
+            }
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            
+
+            //подписки на ивенты
+            client.Log += Client_Log;
+            client.MessageReceived += CommandsHandler;
+
+
+            Console.ReadLine();
+        }
+
+        private  Task CommandsHandler(SocketMessage msg)
+        {
+            switch (msg.Content)
+            {
+                case "!выдай роль":
+                    {
+                        msg.Channel.SendMessageAsync("Выдаю гуля!");
+                        SocketUser author = msg.Author;
+                        break;
+                    }
+                default:
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+        
+    }
+}
